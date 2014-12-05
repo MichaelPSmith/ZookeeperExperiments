b8b5b99af8ab6a2d5c9593be2dfd8af4db3c0796
Mike Smith <mike.smith@codethink.co.uk>
2014-12-05 14:15:26 +0000

    all the files needed should be added this time

---
+++ zkServerFileSetup/Release/src/zkServerFileSetup.d
@@ -0,0 +1,15 @@
+src/zkServerFileSetup.d: ../src/zkServerFileSetup.cpp \
+ /usr/include/zookeeper/zookeeper.h /usr/include/zookeeper/proto.h \
+ /usr/include/zookeeper/zookeeper_version.h \
+ /usr/include/zookeeper/recordio.h \
+ /usr/include/zookeeper/zookeeper.jute.h
+
+/usr/include/zookeeper/zookeeper.h:
+
+/usr/include/zookeeper/proto.h:
+
+/usr/include/zookeeper/zookeeper_version.h:
+
+/usr/include/zookeeper/recordio.h:
+
+/usr/include/zookeeper/zookeeper.jute.h:
