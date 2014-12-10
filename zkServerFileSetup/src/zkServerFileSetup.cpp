75d83043c280bccb56a758fd4449297635885ed1
Mike Smith <mike.smith@codethink.co.uk>
2014-12-10 17:17:30 +0000

    cleaning up repo, added Apache licence to projects

--- zkServerFileSetup/src/zkServerFileSetup.cpp
+++ zkServerFileSetup/src/zkServerFileSetup.cpp
@@ -8,10 +8,24 @@
 
 //============================================================================
 // Name        : ZKTest.cpp
-// Author      : Michael P Smith (AKA, Krin), Under Codethink .Ltd
+// Author      : Michael P Smith (AKA, Krin).
 // Version     : 0.0.1
-// Copyright   : Your copyright notice
-// Description : a first attempt at making a zookeeper client in C
+// Copyright   :
+// 		Copyright [2014] [Codethink Ltd.]
+//
+// 		Licensed under the Apache License, Version 2.0 (the "License");
+// 		you may not use this file except in compliance with the License.
+// 		You may obtain a copy of the License at
+//
+//    		http://www.apache.org/licenses/LICENSE-2.0
+//
+// 		Unless required by applicable law or agreed to in writing, software
+// 		distributed under the License is distributed on an "AS IS" BASIS,
+// 		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 		See the License for the specific language governing permissions and
+// 		limitations under the License.
+//
+// Description : Setup program for files of zookeeper server.
 //============================================================================
 
 #include <iostream>
